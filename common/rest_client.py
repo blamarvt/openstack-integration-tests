e120d1a13732d3489c4ceb595643a27b0a408ff5
Daryl Walleck <daryl.walleck@rackspace.com>
2011-11-02 02:22:15 -0500

    Initial import of tests from the Zodiac project. On suggestion from Westmaas, imported tests under the nova directory (final naming TBD) to more quickly get them imported. To run these tests, execute 'nosetests nova/tests'. I've also only submitted the most stable of the tests. More to come.
    Change-Id: I2abd961992c02b27c4deaa9f11a49ba91c5b765d
    
    Fixed config defaults
    
    Change-Id: I90d5ea20167caddbec6b4cf51a0df9bb333514cb

---
+++ common/rest_client.py
@@ -0,0 +1,91 @@
+import httplib2
+import json
+import nova.config
+
+class RestClient(object):
+    
+    def __init__(self, user, key, auth_url, tenant_name=None):
+        self.config = nova.config.OpenStackConfig()
+        
+        if self.config.env.authentication == 'keystone':
+            self.token, self.base_url = self.keystone_authenticate(user, 
+                                                                   key, 
+                                                                   auth_url, 
+                                                                   tenant_name)
+        else:
+            self.token, self.base_url = self.keystone_authenticate(user, 
+                                                                   key, 
+                                                                   auth_url)
+        
+    def basic_authenticate(self, user, api_key):        
+        """
+        Provides authenitication for the target API
+        """
+        
+        params = {}
+        params['headers'] = {'User-Agent': 'Test-Client', 'X-Auth-User': user, 'X-Auth-Key': api_key}
+
+        self.http_obj = httplib2.Http()
+        resp, body = self.http_obj.request(self.auth_url, 'GET', **params)
+        try:
+            return resp['x-auth-token'], resp['x-server-management-url']
+        except:
+            raise
+            
+    def keystone_authenticate(self, user, api_key, auth_url, tenant_name):        
+        """
+        Provides authenitication via Keystone
+        """
+        
+        creds = { 'auth' : {
+                'passwordCredentials' : {
+                    'username' : user,
+                    'password' : api_key,
+                },
+                'tenantName': tenant_name
+            }
+        
+        }
+        
+        self.http_obj = httplib2.Http()
+        headers = {'Content-Type': 'application/json'}
+        body = json.dumps(creds)
+        resp, body = self.http_obj.request(auth_url,'POST',headers=headers, body=body)
+        
+        try:
+            auth_data = json.loads(body)['access']
+            token = auth_data['token']['id']
+            mgmt_url = auth_data['serviceCatalog'][0]['endpoints'][0]['publicURL']
+            
+            #TODO (dwalleck): This is a horrible stopgap. Need to join strings more cleanly
+            temp = mgmt_url.rsplit('/')
+            management_url = temp[0] + '//' + temp[2] + '/' + temp[3] + '/' + tenant_name
+            return token, management_url
+        except KeyError:
+            print "Failed to authenticate user"
+            raise
+            
+    def post(self, url, body, headers):
+        return self.request('POST', url, headers, body)
+        
+    def get(self, url):
+        return self.request('GET', url)
+        
+    def delete(self, url):
+        return self.request('DELETE', url)
+        
+    def put(self, url, body, headers):
+        return self.request('PUT', url, headers, body)
+        
+    def request(self, method, url, headers=None, body=None):
+        """ A simple HTTP request interface."""
+        
+        self.http_obj = httplib2.Http()
+        if headers == None:
+            headers = {}
+        headers['X-Auth-Token'] = self.token
+        
+        req_url = "%s/%s" % (self.base_url, url)    
+        resp, body = self.http_obj.request(req_url, method, headers=headers, body=body)
+        
+        return resp, body
---
+++ nova/openstack.py
@@ -0,0 +1,37 @@
+from nova.services.nova.json import images_json_client
+from nova.services.nova.json import flavors_client
+from nova.services.nova.json import servers_json_client
+import nova.config
+
+class Manager(object):
+
+    def __init__(self):
+        """
+        Top level manager for all Openstack APIs
+        """
+        
+        self.config = nova.config.OpenStackConfig()
+        if self.config.env.authentication == 'keystone':
+            self.servers_client = servers_json_client.ServersClient(self.config.nova.username, 
+                                                                    self.config.nova.api_key, 
+                                                                    self.config.nova.auth_url, 
+                                                                    self.config.nova.tenant_name)
+            self.flavors_client = flavors_client.FlavorsClient(self.config.nova.username, 
+                                                               self.config.nova.api_key, 
+                                                               self.config.nova.auth_url, 
+                                                               self.config.nova.tenant_name)
+            self.images_client = images_json_client.ImagesClient(self.config.nova.username, 
+                                                                 self.config.nova.api_key, 
+                                                                 self.config.nova.auth_url, 
+                                                                 self.config.nova.tenant_name)
+        else:
+            #Assuming basic/native authentication
+            self.servers_client = servers_json_client.ServersClient(self.config.nova.username, 
+                                                                    self.config.nova.api_key, 
+                                                                    self.config.nova.auth_url)
+            self.flavors_client = flavors_client.FlavorsClient(self.config.nova.username, 
+                                                               self.config.nova.api_key, 
+                                                               self.config.nova.auth_url)
+            self.images_client = images_json_client.ImagesClient(self.config.nova.username, 
+                                                                 self.config.nova.api_key, 
+                                                                 self.config.nova.auth_url)
